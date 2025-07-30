2c84a450a7db4f8d4ccb668660da7fcea500bc82
Neil Bhide <neil@bhide.com>
2025-07-30 16:42:09 -0400

    Added projectile Class

--- src/objects.py
+++ src/objects.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from pygame.locals import *
 
 
@@ -7,12 +8,11 @@ RED = (255,0,0)
 class GameObject(pygame.sprite.Sprite):
     gameObjects = []
 
-    def __init__(self, rect):
+    def __init__(self):
         GameObject.gameObjects.append(self)
-        self.rect = rect
     
-    def draw(self, surface):
-        pass
+    def draw(self, surface, camera_position):
+        pygame.draw.rect(surface, (0, 0, 0), pygame.Rect(self.rect.x + camera_position[0], self.rect.y + camera_position[1], self.rect.width, self.rect.height))
 
     def update(self, dt):
         pass
@@ -21,19 +21,68 @@ class Boss(GameObject):
 
     def __init__(self):
 
+        
+        super().__init__()
 
         self.image = pygame.Surface([100,100])
         self.image.fill(RED)
 
         self.rect = pygame.image.get_rect()
 
-        super().__init__(self.rect)
 
         self.xVel = 0
         self.yVel = 0
 
-    def circularProjectileAttack(self, qty, projectile):
-        for i in range(qty):
-            projectile
+    #def circularProjectileAttack(self, qty, projectile):
 
 class Projectile(GameObject):
+    def __init__(self, magnitude, direction, pos):
+
+        super().__init__()
+
+        self.image = pygame.Surface([50,50])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+        
+
+        self.directionRadians = direction * (math.pi / 180.0)
+        self.xVel = magnitude * math.sin(self.directionRadians)
+        self.yVel = magnitude * math.cos(self.directionRadians)
+
+    def update(self, dt):
+        self.rect.x += self.xVel * dt
+        self.rect.y += self.yVel * dt
+
+class Player(GameObject):
+    SPEED = 400
+
+    def __init__(self):
+        super().__init__()
+        self.rect = pygame.Rect(0, 0, 70, 50)
+        self.xv = 0
+        self.yv = 0
+
+    def update(self, dt):
+        self.rect.x += self.xv * dt
+        self.rect.y += self.yv * dt
+        
+        keys = pygame.key.get_pressed()
+
+        if keys[K_UP]:
+            self.yv = -Player.SPEED
+        elif keys[K_DOWN]:
+            self.yv = Player.SPEED
+        else:
+            self.yv = 0
+
+        if keys[K_LEFT]:
+            self.xv = -Player.SPEED
+        elif keys[K_RIGHT]:
+            self.xv = Player.SPEED
+        else:
+            self.xv = 0
