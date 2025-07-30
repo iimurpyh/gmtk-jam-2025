f279a05934038437b6b515b255567169f84585d7
Neil Bhide <neil@bhide.com>
2025-07-30 15:56:07 -0400

    Added boss class

--- src/objects.py
+++ src/objects.py
@@ -1,6 +1,9 @@
 import pygame
 from pygame.locals import *
 
+
+RED = (255,0,0)
+
 class GameObject(pygame.sprite.Sprite):
     gameObjects = []
 
@@ -13,3 +16,24 @@ class GameObject(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
+
+class Boss(GameObject):
+
+    def __init__(self):
+
+
+        self.image = pygame.Surface([100,100])
+        self.image.fill(RED)
+
+        self.rect = pygame.image.get_rect()
+
+        super().__init__(self.rect)
+
+        self.xVel = 0
+        self.yVel = 0
+
+    def circularProjectileAttack(self, qty, projectile):
+        for i in range(qty):
+            projectile
+
+class Projectile(GameObject):
